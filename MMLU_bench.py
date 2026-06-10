e10f3121cde871a9de053e9a414323af7e71ca9c
ssymon8 <zamouilllle@gmail.com>
2026-06-10 19:22:00 +0200

    print it

--- MMLU_bench.py
+++ MMLU_bench.py
@@ -85,6 +85,7 @@ class MMLUBench:
                 return_tensors="pt",
                 return_dict=True
             ).to(self.device)
+
             with torch.no_grad():
                 outputs = self.model(
                     input_ids=inputs["input_ids"],
@@ -96,8 +97,10 @@ class MMLUBench:
             
             input_length = inputs["input_ids"].shape[1]
 
-            logits = outputs.logits[:, -1, :]
+            logits = outputs.logits[:, :, :]
+            print(f"Logits shape: {logits.shape}")
             choice_logits = logits[:, self.choice_ids] #we isolate the logits corresponding to the tokens for A, B, C, D
+            print(f"Logits for choices A, B, C, D: {choice_logits}")
 
             answer_id = torch.argmax(choice_logits, dim=-1).item()
 
@@ -129,8 +132,7 @@ def main():
         mmlu_bench = MMLUBench(MODEL_ID)
 
         #copied those directly from the MMLU dataset card on HuggingFace
-        subjects = ['abstract_algebra', 'anatomy', 'astronomy', 'business_ethics', 'clinical_knowledge', 'college_biology', 'college_chemistry', 'college_computer_science', 'college_mathematics', 'college_medicine', 'college_physics', 'computer_security', 'conceptual_physics', 'econometrics', 'electrical_engineering', 'elementary_mathematics', 'formal_logic', 'global_facts', 'high_school_biology', 'high_school_chemistry', 'high_school_computer_science', 'high_school_european_history', 'high_school_geography', 'high_school_government_and_politics', 'high_school_macroeconomics', 'high_school_mathematics', 'high_school_microeconomics', 'high_school_physics', 'high_school_psychology', 'high_school_statistics', 'high_school_us_history', 'high_school_world_history', 'human_aging', 'human_sexuality', 'international_law', 'jurisprudence', 'logical_fallacies', 'machine_learning', 'management', 'marketing', 'medical_genetics', 'miscellaneous', 'moral_disputes', 'moral_scenarios', 'nutrition', 'philosophy', 'prehistory', 'professional_accounting', 'professional_law', 'professional_medicine', 'professional_psychology', 'public_relations', 'security_studies', 'sociology', 'us_foreign_policy', 'virology', 'world_religions']
-
+        subjects = ['abstract_algebra']
         scores = []
         for subject in subjects:
             logger.info(f"Evaluating subject: {subject}")
